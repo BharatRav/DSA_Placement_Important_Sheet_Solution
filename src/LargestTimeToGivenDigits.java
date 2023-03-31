9b87d3165af2967abbbe4da6c0e6087e12e319ee
Bharat Rav <bharatrav8077@gmail.com>
2023-03-31 19:51:50 +0530

    added

--- src/LargestTimeToGivenDigits.java
+++ src/LargestTimeToGivenDigits.java
@@ -1,6 +1,26 @@
 import java.util.Arrays;
 //Link:-https://my.newtonschool.co/playground/code/n3qqcmhme817/
 public class LargestTimeToGivenDigits {
+
+    //method 1
+    /*
+    public String largestTimeFromDigits(int[] a) {
+        LinkedList<String> q = new LinkedList<>();
+        q.add("");
+        for (int n : a)
+            for (int size = q.size(); size > 0; size--) {
+                String s = q.poll();
+                for (int i = 0; i <= s.length(); i++)
+                    q.add(s.substring(0, i) + n + s.substring(i));
+            }
+        String largest = "";
+        for (String s : q) {
+            s = s.substring(0, 2) + ":" + s.substring(2);
+            if (s.charAt(3) < '6' && s.compareTo("24:00") < 0 && s.compareTo(largest) > 0)
+                largest = s;
+        }
+        return largest;
+     */
     public String largestTimeFromDigits(int[] A) {
         // enter your code here.
         String time ="";
