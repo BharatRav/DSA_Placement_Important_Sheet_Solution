3a17d24cee3e0166b0ffaaf1bdcabb94d79d111f
Bharat Rav <bharatrav8077@gmail.com>
2023-04-04 22:48:56 +0530

    added

--- src/SwapAdjacentInLRAdjacent.java
+++ src/SwapAdjacentInLRAdjacent.java
@@ -46,6 +46,7 @@ public class SwapAdjacentInLRAdjacent {
 }
 /*
 Problem Statement
+
 In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". Given the starting string start and the ending string end, return True if and only if there exists a sequence of moves to transform one string to the other.
 Input
 User Task:
